c95b60555e3588bd05f6d3c4651b77bcc3c62389
Adam Wall <agcwall@gmail.com>
2019-02-19 23:00:36 -0700

    a clickable button

--- src/backend.rs
+++ src/backend.rs
@@ -1,13 +1,13 @@
-extern crate piston;
-extern crate graphics;
 extern crate glutin_window;
+extern crate graphics;
 extern crate opengl_graphics;
+extern crate piston;
 
-use piston::window::WindowSettings;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::*;
 use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
+use piston::window::WindowSettings;
 
 pub struct Time {
     pub dt: f64,
@@ -15,37 +15,64 @@ pub struct Time {
 
 pub struct Screen<'a> {
     gl: &'a mut GlGraphics,
-    args: RenderArgs,
     context: graphics::Context,
+    pub size: ScreenSize,
 }
+
+pub enum Event {
+    Click(Point),
+}
+
+pub type Color = [f32; 4];
+pub type Point = (f64, f64);
+pub type ScreenSize = Point;
+pub type Rectangle = (Point, Point);
 
 impl<'a> Screen<'a> {
     pub fn draw_box_at_angle(&mut self, angle: f64) {
         use graphics::*;
 
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
-        let (x, y) = (self.args.width / 2.0, self.args.height / 2.0);
+        let (x, y) = (self.size.0 / 2.0, self.size.1 / 2.0);
 
-        let transform = self.context.transform.trans(x, y)
-                                              .rot_rad(angle)
-                                              .trans(-25.0, -25.0);
+        let transform = self
+            .context
+            .transform
+            .trans(x, y)
+            .rot_rad(angle)
+            .trans(-25.0, -25.0);
 
         rectangle(RED, square, transform, self.gl);
     }
+
+    pub fn draw_box(&mut self, color: Color, ((x0, y0), (x1, y1)): Rectangle) {
+        use graphics::*;
+
+        let square = rectangle::rectangle_by_corners(x0, y0, x1, y1);
+        let transform = self
+            .context
+            .transform
+            .trans(0.0, 0.0);
+
+        rectangle(color, square, transform, self.gl);
+    }
 }
 
-pub fn main_loop<STATE, RF, UF>(state: &mut STATE, render_func: RF, update_func: UF) 
-    where RF: Fn(&STATE, Screen), UF: Fn(&mut STATE, Time)
+pub fn main_loop<STATE, RF, UF, EF>(title: &str, state: &mut STATE, render_func: RF, update_func: UF, event_func: EF)
+where
+    RF: Fn(Screen, &STATE),
+    UF: Fn(Time, &mut STATE),
+    EF: Fn(Event, ScreenSize, &mut STATE),
 {
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
+    let mut size = (200.0, 200.0);
+
+    let mut window: Window = WindowSettings::new(title, [size.0, size.1])
         .opengl(opengl)
+        .vsync(true)
         .exit_on_esc(true)
         .build()
         .unwrap();
@@ -53,24 +80,50 @@ pub fn main_loop<STATE, RF, UF>(state: &mut STATE, render_func: RF, update_func:
     let mut gl = GlGraphics::new(opengl);
     let mut events = Events::new(EventSettings::new());
 
+    let mut down = false;
+    let mut last_pos = None;
+
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             use graphics::*;
 
-            const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
+            const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
             gl.draw(r.viewport(), |c, gl| {
+                size = (r.width, r.height);
                 clear(BLACK, gl);
-                render_func(state, Screen {
-                    gl: gl,
-                    args: r,
-                    context: c,
-                });
+                render_func(
+                    Screen {
+                        gl: gl,
+                        size: (r.width, r.height),
+                        context: c,
+                    },
+                    state,
+                );
             });
         }
 
         if let Some(u) = e.update_args() {
-            let time = Time { dt: u.dt };
-            update_func(state, time);
+            update_func(Time { dt: u.dt }, state);
         }
-    };
+
+        if let Some(button) = e.press_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                down = true;
+            }
+        }
+
+        if let Some(button) = e.release_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                down = false;
+                if let Some(pos) = last_pos {
+                    let event = Event::Click(pos);
+                    event_func(event, size, state);
+                }
+            }
+        }
+
+        if let Some(pos) = e.mouse_cursor_args() {
+            last_pos = Some((pos[0], pos[1]));
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 mod backend;
 use backend::*;
 
-fn draw(mut screen: Screen, angle: f64) {
-   screen.draw_box_at_angle(angle)
+fn bounds((x, y): ScreenSize) -> Rectangle {
+    ((x - 80.0, y - 80.0), (x - 20.0, y - 20.0))
+}
+
+fn inside(((x0,y0),(x1,y1)): Rectangle, (xp,yp): Point) -> bool {
+    x0 <= xp && xp < x1 && y0 <= yp && yp < y1
+}
+
+fn draw(mut screen: Screen, on: bool) {
+    const RED: Color = [1.0, 0.0, 0.0, 1.0];
+    const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
+
+    screen.draw_box(
+        if on { GREEN } else { RED },
+        bounds(screen.size),
+    );
+}
+
+fn handle_click(Event::Click(pos): Event, size: ScreenSize, on: &mut bool) {
+    if inside(bounds(size), pos) {
+        *on = !*on;
+    }
 }
 
 fn main() {
-  let mut rotation = 0.0;
+    let mut on = true;
 
-  backend::main_loop(&mut rotation, 
-    |r, scrn| draw(scrn, *r),
-    |r, time| *r += 2.0 * time.dt,
-  );
+    main_loop(
+        "rudiger",
+        &mut on,
+        |scrn, s| draw(scrn, *s),
+        |time, s| (),
+        |evnt, sz, s| handle_click(evnt, sz, s),
+    );
 }
